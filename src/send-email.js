60ec0e35a357fab41912a55f66c23f2f161c0dda
ZehCoque <jose.coque@hotmail.com>
2021-09-22 13:37:18 -0300

    prettier

--- src/send-email.js
+++ src/send-email.js
@@ -2,34 +2,33 @@ const aws = require("aws-sdk");
 const ses = new aws.SES();
 
 const headers = {
-    "Access-Control-Allow-Headers" : "Content-Type",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS,POST"
-  };
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS,POST",
+};
 
 exports.handler = async function (event, context, callback) {
+  const body = JSON.parse(event.body);
 
-    const body = JSON.parse(event.body);
+  const ToAddresses = body.ToAddresses;
+  const Source = body.Source;
+  const Text = { Data: body.Text };
+  const Subject = { Data: body.Subject };
 
-    const ToAddresses = body.ToAddresses;
-    const Source = body.Source;
-    const Text = {Data: body.Text};
-    const Subject = {Data: body.Subject};
-
-    const params = {
+  const params = {
     Destination: {
-      ToAddresses
+      ToAddresses,
     },
     Message: {
       Body: {
-        Text
+        Text,
       },
 
-      Subject
+      Subject,
     },
-    Source
+    Source,
   };
- 
+
   const email = await ses.sendEmail(params).promise();
 
   return callback(null, {
